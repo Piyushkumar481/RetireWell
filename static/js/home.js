3850d63b2bcf51e7a95eb8d1cbfd1d598d4c1816
achlbatra <anchalbatra49@gmail.com>
2025-02-23 23:11:35 +0530

    updated files

---
+++ static/js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('signout_button').onclick = () => {
+  window.location.href = '/logout';
+};
+
+document.getElementById('getStartedBtn').onclick = () => {
+  window.location.href = '/signup';
+}
---
+++ static/js/login.js
@@ -0,0 +1,11 @@
+signup = document.getElementById('signup_link')
+
+signup.addEventListener("click", function(){
+    window.location.href='/signup'
+})
+
+home = document.getElementById('home_button')
+
+home.addEventListener("click", function(){
+    window.location.href='/'
+})
